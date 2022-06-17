4f886e283d3fa0cfb87a89d8124089162d7c4205
Povilas <pbrilius@aol.com>
2022-06-17 17:09:56 +0300

    Twitter - social media corproate giant one - CDN Bootstrap CSS3 &* JS scaffolding framework added up.

--- resources/views/web-solution.blade.php
+++ resources/views/web-solution.blade.php
@@ -3,11 +3,18 @@
     <head>
         <meta charset="utf-8">
         <title>Web solution</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     </head>
     <body>
-        <div class="container-fluid">
-            <h1>Web solution</h1>
-            <p>Hello world!</p>
+        <div class="container container-fluid">
+            <div class="container container-lg">
+                <h1>Web solution</h1>
+                <p>Hello world!</p>
+            </div>
+            <div class="container container-lg">
+                <p>Hello supra-world!</p>
+            </div>
         </div>
     </body>
 </html>
